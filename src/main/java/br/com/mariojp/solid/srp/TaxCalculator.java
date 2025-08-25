d45ea4f612e17fe72dd034c03fa6c466363a02fa
Yasmin <yasminrodrigues.barros@ucsal.edu.br>
2025-08-25 16:53:33 -0300

    TaxCalculator - calcula o imposto com base na taxa

---
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -0,0 +1,15 @@
+package br.com.mariojp.solid.srp;
+
+public class TaxCalculator {
+
+	private double taxRate;
+
+	public double getTaxRate() {
+		return taxRate;
+	}
+
+	public void setTaxRate(double taxRate) {
+		this.taxRate = taxRate;
+	}
+
+}
